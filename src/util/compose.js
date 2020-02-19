e0f7084e82a8c5f5836e545070ea47b31f5cbdbe
Richard Kichenama <rkichenama@gmail.com>
2020-02-18 21:37:01 -0500

    compose

--- src/util/compose.js
+++ src/util/compose.js
@@ -1,14 +1,3 @@
-// export const compose = (...fns) => {
-//   if (fns.length === 0) {
-//     return arg => arg
-//   }
-
-//   if (fns.length === 1) {
-//     return fns[0]
-//   }
-
-//   return fns.reduce((a, b) => (...args) => a(b(...args)))
-// };
 export const compose = (...fns) => (
   fns.length
     ? fns.length > 1
